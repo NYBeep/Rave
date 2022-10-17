293c940e01f265da51f3bceebc5d129411e51f3d
kaiserdis <112853349+kaiserdis@users.noreply.github.com>
2022-10-17 11:06:18 +0300

    Add files via upload

---
+++ sektordiagramm.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+
+plt.pie(y, labels = mylabels, colors = mycolors)
+plt.show() 
