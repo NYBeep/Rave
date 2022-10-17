2b267b412b21c15ef7d7c9e61c7b09d8f60878a3
kaiserdis <112853349+kaiserdis@users.noreply.github.com>
2022-10-17 11:05:02 +0300

    Add files via upload

---
+++ basic graafikl.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4], [1, 11, 9, 16])
+plt.xlabel("siseta nimetus")
+plt.ylabel("sisesta nimetus")
+plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# 
+# ypoints = np.array([3, 8, 1, 10])
+# 
+# plt.plot(ypoints, color = 'r')
+# plt.show()
